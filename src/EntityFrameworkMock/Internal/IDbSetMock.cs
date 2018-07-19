79d1d7d937a188e4501034decd58425c1a090431
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2018-07-19 08:30:03 +0200

    Extend copyright header in other files

--- src/EntityFrameworkMock/Internal/IDbSetMock.cs
+++ src/EntityFrameworkMock/Internal/IDbSetMock.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2017 Wouter Huysentruit
+ * Copyright 2017-2018 Wouter Huysentruit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
