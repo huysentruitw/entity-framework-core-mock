3a566c30647e2cad9b1ef01efc1ba4b9d8fda7ed
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-02-16 19:27:30 +0100

    Update copyright year

--- src/EntityFrameworkCoreMock.NSubstitute/DbSetMock.cs
+++ src/EntityFrameworkCoreMock.NSubstitute/DbSetMock.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2017-2018 Paul Michaels, Wouter Huysentruit
+ * Copyright 2017-2019 Paul Michaels, Wouter Huysentruit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- src/EntityFrameworkCoreMock.Shared/IKeyFactoryBuilder.cs
+++ src/EntityFrameworkCoreMock.Shared/IKeyFactoryBuilder.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2017-2018 Wouter Huysentruit
+ * Copyright 2017-2019 Wouter Huysentruit
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
